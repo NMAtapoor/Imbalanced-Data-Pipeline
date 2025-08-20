b8ff6bd61d80093946030d924f92a7c19dc6b733
Noor Mohammad Atapoor <atapoor88@gmail.com>
2025-08-20 23:17:35 +0100

    small change

--- src/transform/merge_abalone_data_versions.py
+++ src/transform/merge_abalone_data_versions.py
@@ -2,6 +2,5 @@
 
 import pandas as pd
 def merge_abalone_df(df: pd.DataFrame) -> dict[str, pd.DataFrame]:
-    
     merged_abalone_data = pd.concat(df.values(), axis=0)
     return merged_abalone_data
